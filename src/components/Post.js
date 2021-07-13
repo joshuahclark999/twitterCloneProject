00495aeaa33f3be02d08f482e9848f2e440a1f1e
joshuahclark999 <69828377+joshuahclark999@users.noreply.github.com>
2021-07-12 22:28:24 -0400

    trying to add post deleting functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,62 +1,64 @@
-import React from 'react'
-import './css/post.css'
-import {Avatar} from '@material-ui/core';
-import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import React from "react";
+import "./css/post.css";
+import { Avatar } from "@material-ui/core";
+import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
 import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
-import DeleteIcon from '@material-ui/icons/Delete';
-
+import DeleteIcon from "@material-ui/icons/Delete";
+import db from "../firebase";
+const posts = db.collection('posts');
 function Post({
-    displayName,
-    userName,
-    verified,
-    timestamp,
-    content,
-    img,
-    avatar,
-    id
+  displayName,
+  userName,
+  verified,
+  timestamp,
+  content,
+  img,
+  avatar,
+  id,
 }) {
-    
-    function deletePost(e) {
-        console.log(e)
-    }
-    return (
-        <div className="post">
-            <div className="post_avatar">
-                <Avatar src={avatar} />
+  function deletePost() {
+    // db.collection("posts").doc("kaJMxZZ9C5YgZBbo26uT").delete();
+    console.log(posts)
+  }
+  
+  return (
+    <div className="post">
+      <div className="post_avatar">
+        <Avatar src={avatar} />
+      </div>
+      <div className="post_body">
+        <div className="post_body">
+          <div className="post_header">
+            <div className="post_headerText">
+              <h3>
+                {displayName}
+                <span className="post_headerSpecial">
+                  {verified && <VerifiedUserIcon className="post_badge" />}@
+                  {userName}
+                </span>
+              </h3>
             </div>
-            <div className="post_body">
-                 <div className="post_body">
-                     <div className="post_header">
-                         <div className="post_headerText">
-                             <h3>{displayName}
-                                <span className="post_headerSpecial">
-                                    {verified && <VerifiedUserIcon className="post_badge"/>}
-                                    @{userName}
-                                 </span>
-                                 
-                            </h3>
-                         </div>
-                         <div className="post_headerDescription">
-                             <p>{content}</p>
-                             
-                         </div>
-                     </div>
-                     {/* <img src={img} alt="" /> */}
-                    <div className="post_footer">
-                        <ChatBubbleOutlineIcon fontSize="small"/>
-                        <RepeatIcon fontSize="small" />
-                        <FavoriteBorderIcon fontSize="small"/>
-                        <DeleteIcon 
-                        fontSize="small" 
-                        // onClick={}
-                        className="deleteBtn" />
-                    </div>                 
-                 </div>
+            <div className="post_headerDescription">
+              <p>{content}</p>
             </div>
+          </div>
+          {/* <img src={img} alt="" /> */}
+          <div className="post_footer">
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <DeleteIcon
+              fontSize="small"
+              onClick={deletePost}
+              className="deleteBtn"
+            />
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Post
+export default Post;
